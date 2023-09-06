09423ef8d61694477fd52a290bdabcfa032860f9
George <georgehargreaves5@outlook.com>
2023-09-06 16:05:50 +0100

    Initial Commit

---
+++ SoMo/SoMoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoMoApp.swift
+//  SoMo
+//
+//  Created by George Hargreaves on 06/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SoMoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
